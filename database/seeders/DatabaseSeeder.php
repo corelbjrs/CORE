ff83be370c9ff231484119e069afc6da7205fd16
cocecs <act.undag@gmail.com>
2026-08-19 18:59:56 +0800

    Seeder added to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             ExpertiseSeeder::class,
+            CourseExpertiseSeeder::class,
+            MissOccSeeder::class,
         ]);
     }
 }
